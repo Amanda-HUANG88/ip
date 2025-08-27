a1160c65b027ef3cac25a9eeabf5928564fe38f2
lingru <amandahuang588@outlook.com>
2025-08-28 01:21:44 +0800

    Add Increment: Level-1 Echo

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
         System.out.println("————————————————————————————————————————");
         System.out.println("Hello! I'm Luna");
         System.out.println("What can I do for you?");
         System.out.println("————————————————————————————————————————");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("————————————————————————————————————————");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println("————————————————————————————————————————");
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("————————————————————————————————————————");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("————————————————————————————————————————");
+            }
+        }
     }
 }
 
