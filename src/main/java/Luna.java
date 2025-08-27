9c9154ddfab9d4ca89ea92756a6df7e44b7193b2
lingru <amandahuang588@outlook.com>
2025-08-28 01:35:08 +0800

    Add Increment: Level-2 Add, List

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -2,26 +2,35 @@ import java.util.Scanner;
 
 public class Luna {
     public static void main(String[] args) {
-        System.out.println("————————————————————————————————————————");
-        System.out.println("Hello! I'm Luna");
-        System.out.println("What can I do for you?");
-        System.out.println("————————————————————————————————————————");
+        System.out.println("\t————————————————————————————————————————");
+        System.out.println("\tHello! I'm Luna");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t————————————————————————————————————————");
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
         String input;
 
         while (true) {
             input = scanner.nextLine();
-            System.out.println("————————————————————————————————————————");
+            System.out.println("\t————————————————————————————————————————");
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("————————————————————————————————————————");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t————————————————————————————————————————");
                 break;
-            } else {
-                System.out.println(input);
-                System.out.println("————————————————————————————————————————");
+            } else if (input.equals("list")) {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("\t" + (i + 1) + ". " + tasks[i]);
+                    }
+                }else{
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("\tadded: " + input);
+            }
+            System.out.println("\t————————————————————————————————————————");
             }
         }
-    }
 }
 
